1e012ef3b21c90d9b556b183c24ddc6da2826ddb
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-11-01 15:28:46 -0400

    Parsing song JSON

--- main.go
+++ main.go
@@ -21,9 +21,15 @@ func main() {
 		os.Setenv("AccessBearer", string(accessBearer))
 	}
 
-	nowSong := getReturn("me/player/currently-playing")
+	songID, songName := getSong()
+	fmt.Println(songID)
+	fmt.Println(songName)
+}
 
-	fmt.Println(nowSong)
+func getSong() (string, string) {
+	song := getReturn("me/player/currently-playing")
+	item := song["item"].(map[string]interface{})
+	return item["id"].(string), item["name"].(string)
 }
 
 func getReturn(endpoint string) map[string]interface{} {
@@ -43,50 +49,12 @@ func getReturn(endpoint string) map[string]interface{} {
 	}
 
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-
-	// fmt.Println(resp.StatusCode)
-	// fmt.Println(string(bodyBytes))
-
-	// // 	res := map[string]string{}
-	// // 	json.Unmarshal(bodyBytes, res)
-
-	// return res
-
 	map2b := make(map[string]interface{})
-
 	err = json.Unmarshal(bodyBytes, &map2b)
-
 	if err != nil {
 		panic(err)
 	}
 	return map2b
-}
-
-func get(endpoint string) {
-	url := "https://api.spotify.com/v1/" + endpoint
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Authorization", os.Getenv("AccessBearer"))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == 401 {
-		refresh()
-		get(endpoint)
-	}
-
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println(resp.StatusCode)
-	fmt.Println(string(bodyBytes))
-
-	// 	res := map[string]string{}
-	// 	json.Unmarshal(bodyBytes, res)
-
-	// 	return res
 }
 
 func initialize() {
